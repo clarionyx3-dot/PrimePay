bb2354b2c183ccfe88a52f4f581e8150df2d92d3
clarionyx3-dot <clarionyx3@gmail.com>
2026-05-11 13:05:48 +0500

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,13 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  console.log("Firebase Admin SDK Initialized!");
+} else {
+  admin.app(); // Pehle se chalne wala app istemal karein
+}
 
 module.exports = admin;
